ea9a3bb245911d8d6900d2965187c9909703b174
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-05-28 21:30:28 +0100

    Stable wireframe and dev branch.

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-import _ from 'lodash';
+import * as THREE from 'three';
+import * as ORBIT_CONTROLS from 'three-orbit-controls';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = 'Hello'
-  
-    return element;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+
+const earthRadius = 5;
+const earthGeometry = new THREE.IcosahedronGeometry(earthRadius, 5);
+const earthMaterial = new THREE.MeshBasicMaterial({ side: THREE.BackSide, color: 0x4cff00, wireframe: true });
+const earthSphere = new THREE.Mesh(earthGeometry, earthMaterial);
+scene.add(earthSphere);
+
+
+
+
+// Setup the controls
+const OrbitControls = ORBIT_CONTROLS(THREE);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//controls.update() must be called after any manual changes to the camera's transform
+camera.position.set(9, 9, 9);
+controls.update();
+
+function animate() {
+	requestAnimationFrame(animate);
+
+    // Update and smooth camera.
+	controls.update();
+
+	renderer.render(scene, camera);
 }
-
-document.body.appendChild(component());
+animate();
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   mode: 'development',
-  watch: true,
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
